ad8ef1d5a436fa8521c4cac79da785c4af2a3ab8
Manik <manik.gandotra229@gmail.com>
2017-04-29 12:34:33 +0530

    Change Layout and Add Chat Rooms.

--- chat/views.py
+++ chat/views.py
@@ -7,14 +7,20 @@ from .models import Room
 from django.http import HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
 from django.core.context_processors import csrf
+from django.contrib.auth.decorators import login_required
+
+
 def about(request):
     return render(request, "chat/about.html")
 
+@login_required(login_url='/accounts/login/')
 def new_room(request):
     """
     Randomly create a new room, and redirect to it.
     """
+    
     new_room = None
+
     while not new_room:
         with transaction.atomic():
             label = haikunator.haikunate()
@@ -22,7 +28,14 @@ def new_room(request):
                 continue
             new_room = Room.objects.create(label=label)
     return redirect(chat_room, label=label)
+    '''
+    room = request.GET['room_name']
+    print("This is a new Room Request: Room name",room)
+    return redirect(chat_room, label=room)
+    '''
 
+
+@login_required(login_url='/accounts/login/')
 def chat_room(request, label):
     """
     Room view - show the room, with latest messages.
@@ -32,15 +45,39 @@ def chat_room(request, label):
     """
     # If the room with the given label doesn't exist, automatically create it
     # upon first visit (a la etherpad).
-    room, created = Room.objects.get_or_create(label=label)
+    
+    #room, created = Room.objects.get_or_create(label=label)
+    try:
+        room = Room.objects.get(label=label)
+    except Room.DoesNotExist:
+        room = None
+
+    if room ==None:
+        return render_to_response('includes/error.html')
+
 
     # We want to show the last 50 messages, ordered most-recent-last
-    messages = reversed(room.messages.order_by('-timestamp')[:50])
+    messages = reversed(room.messages.order_by('timestamp')[:30])
+
+    #Finding and loading all the rooms in the database
+    all_rooms = Room.objects.all()
+    '''for r in all_rooms:
+        if r.label != 'accounts':
+            if r.label != 'loggedin':
+                print(r.label)
+    '''
+    username = None
+    if request.user.is_authenticated():
+        username = request.user.username
 
     return render(request, "chat/room.html", {
         'room': room,
         'messages': messages,
+        'all_rooms': all_rooms,
+        'username': username,
     })
+
+
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -60,4 +97,13 @@ def registration_complete(request):
     return render_to_response('registration/registration_complete.html')
 
 def loggedin(request):
-    return render_to_response('registration/loggedin.html')
+    all_rooms = Room.objects.all()
+
+    username = None
+    if request.user.is_authenticated():
+        username = request.user.username
+
+    return render(request,'registration/loggedin.html',{
+            'all_rooms':all_rooms,
+            'username': username,
+        })
--- static/chat.js
+++ static/chat.js
@@ -6,19 +6,31 @@ $(function() {
     chatsock.onmessage = function(message) {
         var data = JSON.parse(message.data);
         var chat = $("#chat")
-        var ele = $('<tr></tr>')
+        var ele = $('<li class="left clearfix"></li>')
+        var ele1 = $('<div class="chat-body "></div>')
+        var ele2 = $('<div class="header" style="text-align: left;"></div>')
+        var new_message = $('<p style="color: green" class="pull-right text-muted">New Message</p>')
 
-        ele.append(
-            $("<td></td>").text(data.timestamp)
+
+        ele2.append(
+            $("<strong  class='chat-participant' ><strong>").text(data.handle)
         )
-        ele.append(
-            $("<td></td>").text(data.handle)
+
+        ele2.append(
+            $('<small class="pull-right text-muted" style="margin-right: 10px;"><small>').text(data.timestamp)
         )
-        ele.append(
-            $("<td></td>").text(data.message)
+        ele2.append(new_message)
+        ele2.append(
+            $("<p class='message-text'></p>").text(data.message)
         )
-        
-        chat.append(ele)
+
+        ele1.append(ele2)
+        ele.append(ele1)
+        chat.prepend(ele)
+        $('#chat').load('chat/room.html', function() {
+
+        });
+
     };
 
     $("#chatform").on("submit", function(event) {
